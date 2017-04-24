1144e4caf89e425e29accbbd1e1d7fe1dbba3c45
Shawn Rakowski <srakowski@live.com>
2017-04-23 20:40:26 -0500

    some minor cleanups

--- LD38.DesktopGL/Properties/AssemblyInfo.cs
+++ LD38.DesktopGL/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LD38.DesktopGL")]
+[assembly: AssemblyTitle("Abyss")]
 [assembly: AssemblyProduct("LD38.DesktopGL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
